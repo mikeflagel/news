857ab88a40b61b31f75b0cb26d9d69cfa6a1980a
mikeflagel <mike.flagel@kellogg.northwestern.edu>
2020-02-23 21:57:16 +0000

    comit 2

--- app.rb
+++ app.rb
@@ -14,9 +14,17 @@ end
 
 get "/news" do
     results = Geocoder.search(params["q"])
-    lat_long = results.first.coordinates
-    "#{lat_long[0]} #{lat_long[1]}"
-
     url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9b0b7dddd56b413ca0cb58b252b3ce41"
     news = HTTParty.get(url).parsed_response.to_hash
+    lat_long = results.first.coordinates 
+    lat = "#{lat_long [0]}"
+    long = "#{lat_long [1]}"
+    forecast = ForecastIO.forecast("#{lat}","#{long}").to_hash
+    @current_temperature = forecast["currently"]["temperature"]
+    @conditions = forecast["currently"]["summary"]
+    @forecast = forecast ["daily"]["data"]
+    @paper = news ["articles"]
+    
+    view "news"
+
 end
